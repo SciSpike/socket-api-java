4b3bb5dfe0389679e92822ad825c57e654d2de9a
JK <jonathan.kamke@gmail.com>
2015-03-19 09:23:09 -0700

    bump

--- cert.sh
+++ cert.sh
@@ -1,5 +1,11 @@
 mkdir -p src/main/resources
 rm src/main/resources/*.pem
-openssl genrsa 2048 > src/main/resources/privatekey.pem
-openssl req -new -key src/main/resources/privatekey.pem -out src/main/resources/csr.pem
-openssl x509 -req -days 3650 -in src/main/resources/csr.pem -signkey src/main/resources/privatekey.pem -out src/main/resources/server-crt.pem
+openssl req \
+    -new \
+    -newkey rsa:4096 \
+    -days 3650 \
+    -nodes \
+    -x509 \
+    -subj "/C=US/ST=Distributed/L=Cloud/O=Genesis/CN=*.amcstealth.com" \
+    -keyout privatekey.pem \
+    -out server-crt.pem
